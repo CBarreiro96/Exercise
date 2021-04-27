abe64a7be4975dea5ccdbb1cb1af043a474e5390
Camilo Barreiro <1845@holbertonschool.com>
2021-04-26 20:57:34 -0500

    modification file

--- HackerRank/learning/SQL/Japanese_Cities_Attributes.sql
+++ HackerRank/learning/SQL/Japanese_Cities_Attributes.sql
@@ -2,6 +2,7 @@
  *============================================= Revising the Select Query I =====================================
  * Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
  * The CITY table is described as follows: 
+ *
  *            CITY
  *  -------------------------
  * | Field     |    Type     |
