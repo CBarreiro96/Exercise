19217d07d4b635b171b60d1b520f02f5ead41550
Camilo Barreiro <1845@holbertonschool.com>
2021-07-20 14:53:42 -0500

    Organice repository

--- Mintic_UDEA/main_1_solution_problem.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-from solution_problem_1 import solution, printVector
-
-if __name__ == '__main__':
-    a, sums = solution()
-    printVector(a, 'Original: ')
-    print("\nsum: ", sums)
