f514a5e455171382dfef3a7069e78f2528a8e147
Camilo Barreiro <1845@holbertonschool.com>
2022-03-16 09:56:53 -0500

    dictionary

--- Script/dictionary.py
+++ Script/dictionary.py
@@ -9,11 +9,17 @@ counts = 0;
 finals = 0;
 keyExpression = [];
 numberList = [];
-Expressions = {'acquaintance':'\"Someone you have met, but do not know well\"','clique':'\"A small group of people who keep others from joining them\"','3':'3-','4':'4-'}
+Expressions = {'acquaintance':'\"someone you have met, but do not know well\"','clique':'\"a small group of people who keep others from joining them\"','endure':'to experience and bear something difficult, painful, or unpleasant',
+                'call the shots':  'to be a person who controls or organizes a situation','give someone/something an/the edge over':'to cause one to be in a favorable or superior position when compared to someone  or something else',
+                'take something/someone at face value':'to accept or trust someone or something based only  on an initial or superficial presentation without taking further proof, verification or investigation into account',
+                'create a (more) level playing field':'create more equitable conditions','offspring':'the young of an animal, or a person\'s children','seedling':'a very young plant that has grown  from a seed','sooner or later':'eventually, at some time in the future',
+                'step by step':'gradully','now and then':'rarely','more or less':'approximately, almost','here and there':'in many different places','once and for all':'completely and finally'}
+
 Vocabulary = {'selfless':'It is activity when you\'re thinking of other people before yourself','steering wheel':'A wheel that you turn to control the direction of a vehicle',
             'struggle':'to try very hard to do something difficult','lung':'The organs in the chest that you use to breathe', 'spread':'to affect or cause to affect more people, a large area, etc.',
             'squeeze':'to press something firmly','flooded':'covered with water','ever':'at any time','even':'emphasizes a surprising fact(hasta, incluso)',
-            'laughing stock':'Someone or something that seems stupid or silly, especially by trying to be serious or important and not succeeding','misfit':'Someone who is not suited to a situation or who is not accepted by other people because their behavior is strange or unusual'}
+            'laughing stock':'Someone or something that seems stupid or silly, especially by trying to be serious or important and not succeeding','misfit':'Someone who is not suited to a situation or who is not accepted by other people because their behavior is strange or unusual',
+            'lie down':'to move from a sitting posisition to a horizaontal position'}
 
 Lists = [Expressions,Vocabulary]
 
